c08ef1697484fa8f0d61d3321558245786cdc325
Colton Crowe <coltontcrowe@rr-dhcp-10-33-7-87.redrover.cornell.edu>
2014-06-10 21:19:50 -0400

    added a reade

--- test.py
+++ test.py
@@ -2,10 +2,17 @@ import numpy
 import csv
 
 
-def main():
+def writecsv():
     with open('eggs.csv','wb') as csvfile:
         eggwriter = csv.writer(csvfile)
         eggwriter.writerow(['spam','2spams'])
 
+def readcsv():
+    with open('eggs.csv','r') as csvfile:
+        eggreader = csv.reader(csvfile)
+        for row in eggreader:
+            print row
+
 if __name__ == "__main__":
-    main()
+    writecsv()
+    readcsv()
