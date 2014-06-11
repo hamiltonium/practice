d7ea28ad64be97df302b2b2357e4fea9c7ad0780
Colton Crowe <coltontcrowe@rr-dhcp-10-33-7-87.redrover.cornell.edu>
2014-06-10 21:07:09 -0400

    added csv stuff

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+import numpy
+import csv
+
 
 def main():
-    print "Hello World"
+    with open('eggs.csv','wb') as csvfile:
+        eggwriter = csv.writer(csvfile)
+        eggwriter.writerow(['spam','2spams'])
 
 if __name__ == "__main__":
     main()
